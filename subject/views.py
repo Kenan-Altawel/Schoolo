a663b0abc4de294d0ae970fd7bd7b70255007046
KenanAL <135022788+KenanAl3@users.noreply.github.com>
2025-06-30 16:39:41 +0300

    subject commet 1

--- subject/views.py
+++ subject/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework import viewsets
+from .models import Subject
+from .serializers import SubjectSerializer
+from .permissions import IsAdminUserOnly
+
+class SubjectViewSet(viewsets.ModelViewSet):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+    permission_classes = [IsAdminUserOnly]
